e3f081b6af50771fa5c14c24135984847ff40a96
dominikusbrian <dominikusbrian@live.com>
2021-01-29 21:00:00 +0800

    updating notebooks

--- artisan_beta/setup.py
+++ artisan_beta/setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ArtisanKit", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="dominikusbrian",
     author_email="dominikusbrian@nyu.com",
     description="A Productivity Toolkit for Craftsman/woman Doing Research in Computational Chemistry, Machine Learning, and Beyond.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/xiangsunlab/Artisan",
+    url="https://github.com/xiangsunlab/ArtisanKit",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
