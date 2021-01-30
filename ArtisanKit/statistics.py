61bb9e5f24d4d3fd5b3254ee59acf4fc72a30841
Domi <dominikusbrian@live.com>
2021-01-30 16:56:19 +0800

    Update statistics.py

--- ArtisanKit/statistics.py
+++ ArtisanKit/statistics.py
@@ -5,13 +5,12 @@
 # This software was written in 2021/01                                                           #
 # by Dominikus Brian <dominikusbrian@nyu.edu>/<dominikusbrian@live.com>  ("the author"),         #                    #
 #                                                                                                #
-# LICENCE                                                                                        #
-# Creative Commons CC0 Public Domain Dedication.                                                 #
-# To the extent possible under law, the author have dedicated all copyright and related          #
-# and neighboring rights to this software to the PUBLIC domain worldwide.                        #
-# This software is distributed without any warranty.                                             #
-# You should have received a copy of the CC0 Public Domain Dedication along with this software.  #
-# If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.                               #
+# GNU GENERAL PUBLIC LICENSE                                                                     #
+# Version 3, 29 June 2007                                                                        #
+#                                                                                                #   
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>                           #
+# Everyone is permitted to copy and distribute verbatim copies                                   #
+# of this license document, but changing it is not allowed.                                      #   
 #                                                                                                #
 # DISCLAIMER                                                                                     #
 # The authors and publishers make no warranties about the software, and disclaim liability       #
